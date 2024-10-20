368efcc2fa907ef5596d0debd9a62c6e36698af3
Om Thakur <iomthakur97@gmail.com>
2024-10-20 13:39:31 +0530

    added OM to respository

---
+++ om.js
@@ -0,0 +1,15 @@
+// Hi This is the main file
+function helloWorld() {
+    console.log("Hello World");
+  }
+  
+  helloWorld();
+  
+  function printName(name) {
+    console.log("Hello " + name);
+  }
+  
+  printName("John Doe");
+  
+  printName("Om Thakur");
+
